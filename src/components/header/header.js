84225fc421dbdca81c143106e8a7014b00746939
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-07 13:36:33 +0630

    Adding Trailer video for our MOB-Movie application.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,7 +11,12 @@ export default function header() {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbarScroll' />
           <Navbar.Collapse id='navbarScroll'>
-              
+              <Nav className='me-auto my-2 my-lg-0' style={{maxHeight: '100px'}} navbarScroll>
+                  <NavLink className='nav-link' to='/'>Home</NavLink>
+                  <NavLink className='nav-link' to='/watchList'>Watch List</NavLink>
+              </Nav>
+              <Button variant='outline-info' className='me-2'>Login</Button>
+              <Button variant='outline-info' className='me-2'>Register</Button>
           </Navbar.Collapse>
    </Navbar>
   )
